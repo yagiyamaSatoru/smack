5e2e3e4434d1622c4c24cfdc63ffd970d4f07295
Hiroshi Matsueda <msetsu2000@gmail.com>
2021-01-27 23:05:49 +0900

    mainUi

--- app/src/main/java/com/example/smack/MainActivity.kt
+++ app/src/main/java/com/example/smack/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.smack
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -41,5 +42,17 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun loginBtnNavClicked(view: View) {
+        
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMessageBtnClicked(view: View){
+
     }
 }
